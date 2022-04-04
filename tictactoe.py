e9c5f52bd176dff8da955d4b0b69318603ae16da
Shirleen19 <YOUR_EMAIL>
2022-04-04 08:58:18 +0300

    password generator python

---
+++ tictactoe.py
@@ -0,0 +1,23 @@
+# Mini Project -Random password Generator!
+
+#Importing the relevant modules
+import random
+#All uppercase password
+password=""
+for i in range(10):
+    i=chr(random.randint(65, 90)).lower()
+    password=str(password) +i
+    print(password)
+
+    # Upper and lowercase password
+    password=""
+    for i in range(5):
+        i=chr(random.randint(65, 90)).lower()
+        password=str(password)+ i + i
+        print(password)
+
+
+
+
+
+
